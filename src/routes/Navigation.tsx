8eb436d73c1b3629c1f011124e42c4661eed4af1
G00Z_G00Z <edygomezgg4@gmail.com>
2022-06-29 18:38:53 -0500

    Simple pagination with no lazyload

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,12 +1,12 @@
 import logo from '../logo.svg'
 import { BrowserRouter } from 'react-router-dom'
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages'
 import {
     Navigate,
     NavLink,
     Route,
     Routes
     } from 'react-router-dom'
-
 
 export const Navigation = () => {
   return (
@@ -17,37 +17,37 @@ export const Navigation = () => {
           <ul>
             <li>
               <NavLink
-                to="/home"
+                to="/lazyload1"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Home
+                Lazyload 1
               </NavLink>
             </li>
             <li>
               <NavLink
-                to="/about"
+                to="/lazyload2"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                About
+                Lazyload 2
               </NavLink>
             </li>
             <li>
               <NavLink
-                to="/users"
+                to="/lazyload3"
                 className={({ isActive }) => (isActive ? "nav-active" : "")}
               >
-                Users
+                Lazyload 3
               </NavLink>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="about" element={<h1>About Page</h1>} />
-          <Route path="users" element={<h1>Users Page</h1>} />
-          <Route path="home" element={<h1>Home Page</h1>} />
+          <Route path="lazyload2" element={<LazyPage1 />} />
+          <Route path="lazyload1" element={<LazyPage2 />} />
+          <Route path="lazyload3" element={<LazyPage3 />} />
 
-          <Route path="/*" element={<Navigate to="/home" replace />} />
+          <Route path="/*" element={<Navigate to="/lazyload1" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
