e7f4eaad286a96af38bfba2d9bd5fe26418eebb9
G00Z_G00Z <edygomezgg4@gmail.com>
2022-07-01 17:53:59 -0500

    Corrected import

--- src/02-component-patterns/components/ProductImage.tsx
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -1,25 +1,20 @@
-import { useContext } from 'react';
-import { ProductContext } from './ProductCard';
-
+import noImage from '../assets/no-image.jpg'
 import styles from '../styles/styles.module.css'
-import noImage from '../assets/no-image.jpg';
+import { ProductContext } from './ProductCard'
+import { useContext } from 'react'
 
 
-export const ProductImage = ({ img = '' }) => {
+export const ProductImage = ({ img = "" }) => {
+  const { product } = useContext(ProductContext);
+  let imgToShow: string;
 
-    const { product } = useContext( ProductContext );
-    let imgToShow: string;
+  if (img) {
+    imgToShow = img;
+  } else if (product.img) {
+    imgToShow = product.img;
+  } else {
+    imgToShow = noImage;
+  }
 
-    if ( img ) {
-        imgToShow = img;
-    } else if ( product.img ) {
-        imgToShow = product.img
-    } else {
-        imgToShow = noImage;
-    }
-
-
-    return (
-        <img className={ styles.productImg } src={ imgToShow } alt="Product" />
-    );
-}
+  return <img className={styles.productImg} src={imgToShow} alt="Product" />;
+};
